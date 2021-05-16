31466e83e88f72163182111abed7e6518eb121fd
mtpullin <mtpullin@gmail.com>
2021-05-16 09:21:09 -0400

    attempting to add images to quiz

--- script.js
+++ script.js
@@ -1,46 +1,66 @@
+var body = document.getElementById("body")
 var timerEl = document.getElementById("countdown");
 var questionEl = document.getElementById("questions");
 var start = document.getElementById("start-quiz");
 var a = document.getElementById("a");
 var b = document.getElementById("b");
 var c = document.getElementById("c");
-var correct = 0;
-var incorrect = 0;
+var score = 0;
 var questions = [
     
-    {
-        Question: "What car is this?",
-        imgsrc: "./images/1967-Chevy-Camaro.jpg",
-        answers:["Chevy Camaro", "Dodge Dart", "Pontiac GTO"],
-        correct: "Chevy Camaro"
+{
+    q: "What car is this?",
+    choices:["Chevy Camaro", "Dodge Dart", "Pontiac GTO"],
+    a: "Chevy Camaro"
 },
     
-{   Question: "What car is this?",
-    imagesrc: "./images/1968-ford-mustang.jpg",
-    answers: ["Dodge Charger", "Ford Mustang", "Chevy Camaro"],
-    correct: "Ford Mustang"
+{   q: "What car is this?",
+    choices: ["Dodge Charger", "Ford Mustang", "Chevy Camaro"],
+    a: "Ford Mustang"
     },
     
     {
-        Question: "What car is this?",
-        imgsrc: "./1970-oldsmobile-442.jpg",
-        answers: ["Dodge Camaro", "Chevy Mustang", "Oldsmobile 442"],
-        correct: "Oldsmobile 442"
+        q: "What car is this?",
+        choices: ["Dodge Camaro", "Chevy Mustang", "Oldsmobile 442"],
+        a: "Oldsmobile 442"
     },
     
     {
-        Question: "What car is this?",
-        imgsrc: "./1977-Pontiac-Firebird-Trans-Am-image-685.jpg",
-        answers: ["Chevy Corvette", "Plymouth Baracuda", "Pontiac Firebird"],
-        correct: "Pontiac Firebird"
+        q: "What car is this?",
+        choices: ["Chevy Corvette", "Plymouth Baracuda", "Pontiac Firebird"],
+        a: "Pontiac Firebird"
     },
-    {Question: "What car is this?",
-    imgsrc: "./2017-dodge-challenger-srt-demon.jpg",
-    answers: ["Dodge Demon", "Dodge Charger", "Dodge Dart"],
-    correct: "Dodge Demon"
+    {q: "What car is this?",
+    choices: ["Dodge Demon", "Dodge Charger", "Dodge Dart"],
+    a: "Dodge Demon"
     }
 ]
+var images = [
+    "/1967-Chevy-Camaro.jpg",
+    "./images/1968-ford-mustang.jpg",
+    "./1970-oldsmobile-442.jpg",
+    "./1977-Pontiac-Firebird-Trans-Am-image-685.jpg",
+    "./2017-dodge-challenger-srt-demon.jpg",
+]
 
+
+function quiz() {
+    for (var i = 0; i < questions.length; i++) {
+var answer = prompt(questions[i].q)
+
+if ( answer === questions[i].a) {
+    alert("correct");}
+
+else {
+    alert("wrong!");
+}
+function showimage() {
+    for(var i = 0; i < images.length; i++) {
+        document.appendChild("body")
+        }
+    }start.onclick = showimage;
+}
+}
 //timer countdown function
 function countdown() {
     var timeleft = 60;
@@ -50,10 +70,12 @@ function countdown() {
                 timeleft--;
             }
             else {
-                confirm("you ran out of time");
+                promt("Times up enter initials for score.");
                 clearInterval(timeinterval);
             }
         }, 1000)
     }
-    start.onclick = countdown;
 
+    start.onclick = countdown;
+    start.onclick = quiz;
+    
