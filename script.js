236bdaee43c86ce64a71face82145005baac44cf
mtpullin <mtpullin@gmail.com>
2021-05-16 10:29:54 -0400

    attempting to add answers to buttons

--- script.js
+++ script.js
@@ -6,61 +6,79 @@ var a = document.getElementById("a");
 var b = document.getElementById("b");
 var c = document.getElementById("c");
 var score = 0;
+//var qimages = document.getElementById("images");
 var questions = [
     
 {
     q: "What car is this?",
-    choices:["Chevy Camaro", "Dodge Dart", "Pontiac GTO"],
-    a: "Chevy Camaro"
+    imgSrc: "/images/1967-Chevy-Camaro.jpg",
+    choices:[{a: "Chevy Camaro"}, {b: "Dodge Dart"}, {c: "Pontiac GTO"}],
+    answer: "Chevy Camaro"
 },
     
 {   q: "What car is this?",
-    choices: ["Dodge Charger", "Ford Mustang", "Chevy Camaro"],
-    a: "Ford Mustang"
+    choices: [{a: "Dodge Charger"}, {b: "Ford Mustang"}, {c: "Chevy Camaro"}],
+    answer: "Ford Mustang"
     },
     
     {
         q: "What car is this?",
-        choices: ["Dodge Camaro", "Chevy Mustang", "Oldsmobile 442"],
-        a: "Oldsmobile 442"
+        choices: [{a:"Dodge Camaro"}, {b:"Chevy Mustang"}, {c:"Oldsmobile 442"}],
+        answer: "Oldsmobile 442"
     },
     
     {
         q: "What car is this?",
-        choices: ["Chevy Corvette", "Plymouth Baracuda", "Pontiac Firebird"],
-        a: "Pontiac Firebird"
+        choices: [{a:"Chevy Corvette"}, {b:"Plymouth Baracuda"}, {c: "Pontiac Firebird"}],
+        answer: "Pontiac Firebird"
     },
     {q: "What car is this?",
-    choices: ["Dodge Demon", "Dodge Charger", "Dodge Dart"],
-    a: "Dodge Demon"
+    choices: [{a:"Dodge Demon"}, {b:"Dodge Charger"}, {c:"Dodge Dart"}],
+    answer: "Dodge Demon"
     }
 ]
-var images = [
-    "/1967-Chevy-Camaro.jpg",
+var images = document.getElementById([
+    "./1967-Chevy-Camaro.jpg",
     "./images/1968-ford-mustang.jpg",
     "./1970-oldsmobile-442.jpg",
     "./1977-Pontiac-Firebird-Trans-Am-image-685.jpg",
     "./2017-dodge-challenger-srt-demon.jpg",
-]
+])
+function renderquestion() {
+    let q = questions
 
+    question.innerhtml = "<p>" + q.question +"</p>";
+    images.innerhtml = "<img src="+q.imgSrc +">";
+    a.innerhtml = q.a;
+    b.innerhtml = q.b;
+    c.innerhtml = q.c;
+}
+//function showimage() {
+    //for(var i = 0; i < images.length; i++) {
+        //document.createAttribute("img")
+        //document.appendChild("body")
+        //}
+    //}
 
 function quiz() {
+    countdown();
+    showimage();
+    renderquestion();
     for (var i = 0; i < questions.length; i++) {
-var answer = prompt(questions[i].q)
+var answer = (questions[i].q)
 
 if ( answer === questions[i].a) {
-    alert("correct");}
+    score++;}
 
 else {
     alert("wrong!");
+    time--;
 }
-function showimage() {
-    for(var i = 0; i < images.length; i++) {
-        document.appendChild("body")
-        }
-    }start.onclick = showimage;
+
 }
 }
+
+
 //timer countdown function
 function countdown() {
     var timeleft = 60;
@@ -76,6 +94,5 @@ function countdown() {
         }, 1000)
     }
 
-    start.onclick = countdown;
     start.onclick = quiz;
     
