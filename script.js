0762c71036a9992e26c3bc78d9c697c081ff82f7
mtpullin <mtpullin@gmail.com>
2021-05-17 10:23:53 -0400

    modified indentation

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var body = document.getElementById("body")
+var question = document.getElementById("questions")
 var timerEl = document.getElementById("countdown");
 var questionEl = document.getElementById("quiz");
 var start = document.getElementById("start-quiz");
@@ -9,32 +9,38 @@ var score = 0;
 //var qimages = document.getElementById("images");
 var questions = [
     
-{
-    q: "What car is this?",
-    image: "images/1967-Chevy-Camaro.jpg",
-    choices:[{text: "Chevy Camaro"}, {text: "Dodge Dart"}, {text: "Pontiac GTO"}],
-    answer: "Chevy Camaro"
-},
+    {
+        q: "What car is this?",
+        image: "images/1967-Chevy-Camaro.jpg",
+        choices:[{text: "Chevy Camaro"}, {text: "Dodge Dart"}, {text: "Pontiac GTO"}],
+        a: "Chevy Camaro"
+    },
     
-{   q: "What car is this?",
-    choices: [{text: "Dodge Charger"}, {text: "Ford Mustang"}, {text: "Chevy Camaro"}],
-    answer: "Ford Mustang"
+    {   
+        q: "What car is this?",
+        image: "images/1968-ford-mustang.jpg",
+        choices: [{text: "Dodge Charger"}, {text: "Ford Mustang"}, {text: "Chevy Camaro"}],
+        a: "Ford Mustang"
     },
     
     {
         q: "What car is this?",
+        image:"1970-oldsmobile-442.jpg",
         choices: [{text:"Dodge Camaro"}, {text:"Chevy Mustang"}, {text:"Oldsmobile 442"}],
-        answer: "Oldsmobile 442"
+        a: "Oldsmobile 442"
     },
     
     {
         q: "What car is this?",
+        image:"1977-Pontiac-Firebird-Trans-Am-image.jpg",
         choices: [{text:"Chevy Corvette"}, {text:"Plymouth Baracuda"}, {text: "Pontiac Firebird"}],
-        answer: "Pontiac Firebird"
+        a: "Pontiac Firebird"
     },
-    {q: "What car is this?",
-    choices: [{text:"Dodge Demon"}, {text:"Dodge Charger"}, {text:"Dodge Dart"}],
-    answer: "Dodge Demon"
+    {   
+        q: "What car is this?",
+        image:"2017-dodge-challenger-srt-demon.jpg",
+        choices: [{text:"Dodge Demon"}, {text:"Dodge Charger"}, {text:"Dodge Dart"}],
+        a: "Dodge Demon"
     }
 ]
 
@@ -47,18 +53,19 @@ function quiz() {
     for (var i = 0; i < questions.length; i++) {
 var answer = (questions[i].q)
 
-if ( answer === questions[i].a) {
-    score++;}
+    if ( answer === questions[i].a) {
+        score++;}
 
-else {
-    timeleft-= 5;
+    else {
+        imeleft-= 5;
 }
-function nextQuestion() {
-    showQuestion(questions[i])
+    function nextQuestion() {
+        showQuestion(questions[i])
 }
-function showQuestion(questions) {
-    questions.innerText = questions.questionsEl
+    function showQuestion(questions) {
+        question.innerHTML = questions.questions
 }
+nextQuestion();
 }
 }
 //timer countdown function
@@ -70,8 +77,9 @@ function countdown() {
                 timeleft--;
             }
             else {
-                promt("Times up enter initials for score.");
+                prompt("Times up enter initials for score.");
                 clearInterval(timeinterval);
+                timerEl.classList.add('hide');
             }
         }, 1000)
     }
