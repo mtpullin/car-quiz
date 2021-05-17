74b4080d12b5abd19adc97eb334522c9e3e2f6d8
mtpullin <mtpullin@gmail.com>
2021-05-17 13:45:38 -0400

    still not getting anywhere

--- script.js
+++ script.js
@@ -4,78 +4,80 @@ var questionEl = document.getElementById("quiz");
 var start = document.getElementById("start-quiz");
 var timeleft = 60;
 var title = document.getElementById("title")
-var answerButton = document.getElementById("answer")
+var answerButton = document.getElementById("answers")
 var score = 0;
-var currentQuestionIndex = 0;
 var image = document.getElementById("images")
-
+let shuffledQuestions, currentQuestionIndex
 var questions = [
     
     {
         q: "What car is this?",
         image:"images/1967-Chevy-Camaro.jpg",
-        choices:[{text: "Chevy Camaro"}, {text: "Dodge Dart"}, {text: "Pontiac GTO"}],
-        a: "Chevy Camaro"
+        answers:[{text: "Chevy Camaro", correct: true}, {text: "Dodge Dart", correct: false}, {text: "Pontiac GTO", correct: false}],
     },
     
     {   
         q: "What car is this?",
         image:"images/1968-ford-mustang.jpg",
-        choices: [{text: "Dodge Charger"}, {text: "Ford Mustang"}, {text: "Chevy Camaro"}],
-        a: "Ford Mustang"
+        answers: [{text: "Dodge Charger", correct: false}, {text: "Ford Mustang", correct: true}, {text: "Chevy Camaro", correct: false}],
     },
     
     {
         q: "What car is this?",
         image:"images/1970-oldsmobile-442.jpg",
-        choices: [{text:"Dodge Camaro"}, {text:"Chevy Mustang"}, {text:"Oldsmobile 442"}],
-        a: "Oldsmobile 442"
+        answers: [{text:"Dodge Camaro", correct: false}, {text:"Chevy Mustang", correct: false}, {text:"Oldsmobile 442", correct: true}],
     },
     
     {
         q: "What car is this?",
         image:"images/1977-Pontiac-Firebird-Trans-Am-image.jpg",
-        choices: [{text:"Chevy Corvette"}, {text:"Plymouth Baracuda"}, {text: "Pontiac Firebird"}],
-        a: "Pontiac Firebird"
+        answers: [{text:"Chevy Corvette", correct: false}, {text:"Plymouth Baracuda", correct: false}, {text: "Pontiac Firebird", correct: true}],
     },
     {   
         q: "What car is this?",
         image:"images/2017-dodge-challenger-srt-demon.jpg",
-        choices: [{text:"Dodge Demon"}, {text:"Dodge Charger"}, {text:"Dodge Dart"}],
-        a: "Dodge Demon"
+        answers: [{text:"Dodge Demon", correct: true}, {text:"Dodge Charger", correct: false}, {text:"Dodge Dart", correct: false}],
     }
 ]
 
 
-function quiz() {
+function startquiz() {
     start.classList.add('hide');
     title.classList.add('hide');
     questionEl.classList.remove('hide');
+    shuffledQuestions = questions.sort(()=>Math.random()- .5)
+    currentQuestionIndex = 0;
+    setNextQuestion()
     countdown();
-    for (var i = 0; i < questions.length; i++) {
-var answer = (questions[i].q)
-
-    if ( answer === questions[i].a) {
-        score++;}
-
-    else {
-        timeleft-= 5;
 }
-    
+    function setNextQuestion () {
+        showQuestion(shuffledQuestions[currentQuestionIndex])
+    }
     function displayQuestion() {
-        questions.textContent = questions[i].questions;
-        var optionsBtnsArray = [];
-        var indexArray = [];
+        questions.innerText = questions.q;
+        questions.answers.forEach(answers => {
+            const button = document.createElement('button')
+            button.innertext = answers.text
+            button.classList.add('button')
+            if (answers.correct) {
+                button.dataset.correct = answers.correct
+            }
+            button.addEventListener('click', selectAnswer)
+            answerButton.appendChild(button)
+        function selectAnswer() {
+
+        }
+            
+        });
         var image = document.createElement("img");
         image.setAttribute("src", questions[currentQuestionIndex].image);
         images.append(image);
     }
 function nextQuestion() {
-    displayQuestion()
+    displayQuestion(questions, questionEl);
 }
 nextQuestion();
-}
-}
+
 //timer countdown function
 function countdown() {
     var timeleft = 60;
@@ -92,6 +94,5 @@ function countdown() {
         }, 1000)
     }
 
-    start.onclick = quiz;
-    answerButton.addEventListener("click", assesSelection);
+    start.onclick = startquiz;
     
