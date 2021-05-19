bb79594b4311e458a5a1dcad26b0bdd7c99384d3
mtpullin <mtpullin@gmail.com>
2021-05-19 19:47:20 -0400

    changed to LOTR Movie quiz due to not being able to figure how to insert photos

--- script.js
+++ script.js
@@ -1,82 +1,124 @@
-var question = document.getElementById("questions")
+  
+const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
+const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
+var carImage = document.getElementById('questionImage')
 var timerEl = document.getElementById("countdown");
-var questionEl = document.getElementById("quiz");
-var start = document.getElementById("start-quiz");
-var timeleft = 60;
-var title = document.getElementById("title")
-var answerButton = document.getElementById("answers")
-var score = 0;
-var image = document.getElementById("images")
-let shuffledQuestions, currentQuestionIndex
+var scoreEl = document.getElementById("score");
 var questions = [
     
     {
-        q: "What car is this?",
-        image:"images/1967-Chevy-Camaro.jpg",
-        answers:[{text: "Chevy Camaro", correct: true}, {text: "Dodge Dart", correct: false}, {text: "Pontiac GTO", correct: false}],
+        question: "how many awards did the Lord of the Rings film trilogy earn?",
+        answers:[{text: "17", correct: true}, {text: "30", correct: false}, {text: "2", correct: false}],
     },
     
     {   
-        q: "What car is this?",
-        image:"images/1968-ford-mustang.jpg",
-        answers: [{text: "Dodge Charger", correct: false}, {text: "Ford Mustang", correct: true}, {text: "Chevy Camaro", correct: false}],
+        question: "Who played Frodo Baggins?",
+        answers: [{text: "Orlando Bloom", correct: false}, {text: "Elijah Wood", correct: true}, {text: "Billy Boyd", correct: false}],
     },
     
     {
-        q: "What car is this?",
-        image:"images/1970-oldsmobile-442.jpg",
-        answers: [{text:"Dodge Camaro", correct: false}, {text:"Chevy Mustang", correct: false}, {text:"Oldsmobile 442", correct: true}],
+        question: "What was Galadriel's gift to Gimli?",
+        answers: [{text:"An Axe", correct: false}, {text:"A Kiss", correct: false}, {text:"3 Hairs", correct: true}],
     },
     
     {
-        q: "What car is this?",
-        image:"images/1977-Pontiac-Firebird-Trans-Am-image.jpg",
-        answers: [{text:"Chevy Corvette", correct: false}, {text:"Plymouth Baracuda", correct: false}, {text: "Pontiac Firebird", correct: true}],
+        question: "How many people were a part of the fellowship of the ring?",
+        answers: [{text:"5", correct: false}, {text:"7", correct: false}, {text: "9", correct: true}],
     },
     {   
-        q: "What car is this?",
-        image:"images/2017-dodge-challenger-srt-demon.jpg",
-        answers: [{text:"Dodge Demon", correct: true}, {text:"Dodge Charger", correct: false}, {text:"Dodge Dart", correct: false}],
+        question: "Who actually met JRR Tolkien that was involved in the film?",
+        answers: [{text:"Christopher Lee", correct: true}, {text:"John Rhys-Davies", correct: false}, {text:"Ian McKellen", correct: false}],
     }
 ]
 
+let shuffledQuestions, currentQuestionIndex
 
-function startquiz() {
-    start.classList.add('hide');
-    title.classList.add('hide');
-    questionEl.classList.remove('hide');
-    shuffledQuestions = questions.sort(()=>Math.random()- .5)
-    currentQuestionIndex = 0;
-    setNextQuestion()
-    countdown();
-}
-    function setNextQuestion () {
-        showQuestion(shuffledQuestions[currentQuestionIndex])
-    }
-    function displayQuestion() {
-        questions.innerText = questions.q;
-        questions.answers.forEach(answers => {
-            const button = document.createElement('button')
-            button.innertext = answers.text
-            button.classList.add('button')
-            if (answers.correct) {
-                button.dataset.correct = answers.correct
-            }
-            button.addEventListener('click', selectAnswer)
-            answerButton.appendChild(button)
-        function selectAnswer() {
+startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', () => {
+    countdown()
+    currentQuestionIndex++
+  setNextQuestion()
+  
+})
 
-        }
-            
-        });
-        var image = document.createElement("img");
-        image.setAttribute("src", questions[currentQuestionIndex].image);
-        images.append(image);
-    }
-function nextQuestion() {
-    displayQuestion(questions, questionEl);
+function startGame() {
+  startButton.classList.add('hide')
+  shuffledQuestions = questions.sort(() => Math.random() - .5)
+  currentQuestionIndex = 0
+  questionContainerElement.classList.remove('hide')
+  setNextQuestion()
 }
-nextQuestion();
+
+function setNextQuestion() {
+  resetState()
+  showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+
+function showQuestion(question) {
+  questionElement.innerText = question.question
+  question.answers.forEach(answer => {
+    const button = document.createElement('button')
+    button.innerText = answer.text
+    button.classList.add('btn')
+    if (answer.correct) {
+      button.dataset.correct = answer.correct
+    }
+    button.addEventListener('click', selectAnswer)
+    answerButtonsElement.appendChild(button)
+  })
+}
+
+function resetState() {
+  clearStatusClass(document.body)
+  nextButton.classList.add('hide')
+  while (answerButtonsElement.firstChild) {
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+  }
+}
+
+function selectAnswer(e) {
+  const selectedButton = e.target
+  const correct = selectedButton.dataset.correct
+  setStatusClass(document.body, correct)
+  Array.from(answerButtonsElement.children).forEach(button => {
+    setStatusClass(button, button.dataset.correct)
+  })
+  if (shuffledQuestions.length > currentQuestionIndex + 1) {
+    nextButton.classList.remove('hide')
+  } else {
+    startButton.innerText = 'Restart'
+    startButton.classList.remove('hide')
+  }
+}
+
+function setStatusClass(element, correct) {
+  clearStatusClass(element)
+  if (correct) {
+    element.classList.add('correct')
+  } else {
+    element.classList.add('wrong')
+  }
+}
+
+function clearStatusClass(element) {
+  element.classList.remove('correct')
+  element.classList.remove('wrong')
+}
+function showHighScore() {
+    var score = localStorage.getItem('score')
+    if (score === null)
+    return;
+}
+
+incrementScore = num => {
+    score += num;
+    scoreText.innerText = score; };
+
+
+
 
 //timer countdown function
 function countdown() {
@@ -93,6 +135,4 @@ function countdown() {
             }
         }, 1000)
     }
-
-    start.onclick = startquiz;
     
